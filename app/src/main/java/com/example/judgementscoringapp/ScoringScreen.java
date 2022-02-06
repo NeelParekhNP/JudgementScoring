25c19bb5a93f7951a3c50b02acf6b61ac0dc4820
NeelParekhNP <47748376+NeelParekhNP@users.noreply.github.com>
2022-02-06 16:59:10 +0000

    Added functionality to input number of hands for each player.

--- app/src/main/java/com/example/judgementscoringapp/ScoringScreen.java
+++ app/src/main/java/com/example/judgementscoringapp/ScoringScreen.java
@@ -1,15 +1,20 @@
 package com.example.judgementscoringapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -22,6 +27,9 @@ import java.util.List;
 public class ScoringScreen extends AppCompatActivity {
 
     private ActivityScrollingBinding binding;
+    private String m_Text = "";
+
+    private Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,38 @@ public class ScoringScreen extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
+        doneButton = findViewById(R.id.scoreDoneButton);
+
+        List<String> players = getIntent().getStringArrayListExtra("playersList");
+
+        List<TextView> tableOutput = generateTable(players);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(int i= players.size()-1; i>=0; i--){
+                    inputDialogue(players.get(i), tableOutput.get(i));
+                }
+            }
+        });
+    }
+
+    private List<String> generateInitials(List<String> players){
+        List<String> playerInitials = new ArrayList<>();
+
+        for(String player : players){
+            String initial = player.substring(0,1);
+            if(playerInitials.contains(initial)){
+                playerInitials.add(player.substring(0,2));
+            }
+            else{
+                playerInitials.add(initial);
+            }
+        }
+        return playerInitials;
+    }
+
+    private List<TextView> generateTable(List<String> players){
         List<String> sirs = new ArrayList<>();
         sirs.add("N");
         sirs.add("K");
@@ -42,7 +82,6 @@ public class ScoringScreen extends AppCompatActivity {
         sirs.add("F");
         sirs.add("L");
 
-        List<String> players = getIntent().getStringArrayListExtra("playersList");
         List<String> playerInitials = generateInitials(players);
 
         TableLayout scores = (TableLayout)findViewById(R.id.score_table);
@@ -60,6 +99,7 @@ public class ScoringScreen extends AppCompatActivity {
         scores.addView(tr);
 
         int numberOfGames = 52/players.size();
+        List<TextView> allColumns = new ArrayList<>();
 
         for(int i = 0; i < numberOfGames; i++){
             TableRow row = new TableRow(this);
@@ -69,8 +109,8 @@ public class ScoringScreen extends AppCompatActivity {
             row.addView(firstColumn);
             for(int j = 1; j <= players.size(); j++){
                 TextView column = new TextView(this);
-                column.setText(String.valueOf(new Integer(i +1)));
                 row.addView(column);
+                allColumns.add(column);
             }
             scores.addView(row);
         }
@@ -83,25 +123,40 @@ public class ScoringScreen extends AppCompatActivity {
             row.addView(firstColumn);
             for(int j = 1; j <= players.size(); j++){
                 TextView column = new TextView(this);
-                column.setText(String.valueOf(new Integer(i)));
                 row.addView(column);
+                allColumns.add(column);
             }
             scores.addView(row);
         }
+
+        return allColumns;
     }
 
-    public List<String> generateInitials(List<String> players){
-        List<String> playerInitials = new ArrayList<>();
+    private void inputDialogue(String player, TextView cell){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter number of hands for: " + player);
 
-        for(String player : players){
-            String initial = player.substring(0,1);
-            if(playerInitials.contains(initial)){
-                playerInitials.add(player.substring(0,2));
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cell.setText(input.getText().toString());
+                dialog.cancel();
             }
-            else{
-                playerInitials.add(initial);
+        });
+/*        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
             }
-        }
-        return playerInitials;
+        });*/
+
+        builder.show();
     }
 }
